a0ba8bead727bdd8d041d2d6ae43aafccf35b86a
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-11-03 20:17:37 +0000

    Added Dijkstra algorithm

---
+++ src/graph/Graph.java
@@ -0,0 +1,27 @@
+package graph;
+
+import java.util.List;
+
+import graph.Edge;
+import graph.Vertex;
+
+public class Graph {
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+
+
+}
