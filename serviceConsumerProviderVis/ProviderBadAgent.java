aeb29f0b818bfe5b5d4082844fa31808e60ce762
aquelemiguel <miguelmanofernandes@gmail.com>
2018-11-06 02:46:11 +0000

    Remove unnecessary example folder

--- serviceConsumerProviderVis/ProviderBadAgent.java
+++
@@ -1,9 +0,0 @@
-package serviceConsumerProviderVis;
-
-public class ProviderBadAgent extends ProviderAgent {
-
-	public ProviderBadAgent() {
-		super(RepastSServiceConsumerProviderLauncher.FAILURE_PROBABILITY_BAD_PROVIDER);
-	}
-
-}
--- serviceConsumerProviderVis/ProviderGoodAgent.java
+++
@@ -1,9 +0,0 @@
-package serviceConsumerProviderVis;
-
-public class ProviderGoodAgent extends ProviderAgent {
-
-	public ProviderGoodAgent() {
-		super(RepastSServiceConsumerProviderLauncher.FAILURE_PROBABILITY_GOOD_PROVIDER);
-	}
-
-}
