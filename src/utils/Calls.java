5eae2235f39e49062c7eb082c55c3d03ec79abdf
João Seixas <jnuno1997@gmail.com>
2018-11-07 21:29:12 +0000

    Some progress in Callouts

--- src/utils/Calls.java
+++ src/utils/Calls.java
@@ -1,22 +1,37 @@
 package utils;
 
 import java.util.ArrayList;
-import java.util.List;
-
 import agents.TAgent;
 import repast.simphony.space.grid.GridPoint;
 
 public class Calls<T> {
 
     public static enum Positions {A_SHORT, A_LONG, A_SITE, MID_TO_B, B_TUNNELS, B_SITE}
+    public static enum Callouts {A_SPLIT, A_RUSH, B_SPLIT, B_RUSH, DEFAULT} 
 
-    public class Callouts {
-        Positions[] A_SPLIT = {Positions.A_SHORT, Positions.A_SHORT, Positions.A_LONG, Positions.A_LONG, Positions.A_LONG};
-        Positions[] A_RUSH = {Positions.A_SITE, Positions.A_SITE, Positions.A_SITE, Positions.A_SITE, Positions.A_SITE};
-        Positions[] B_SPLIT = {Positions.MID_TO_B, Positions.MID_TO_B, Positions.MID_TO_B, Positions.B_TUNNELS, Positions.B_TUNNELS};
-        Positions[] B_RUSH = {Positions.B_SITE, Positions.B_SITE, Positions.B_SITE, Positions.B_SITE, Positions.B_SITE};
-        Positions[] DEFAULT = {Positions.B_SITE, Positions.MID_TO_B, Positions.A_SHORT, Positions.A_LONG, Positions.A_SITE};
+   
+    private final Positions[] A_SPLIT = {Positions.A_SHORT, Positions.A_SHORT, Positions.A_LONG, Positions.A_LONG, Positions.A_LONG};
+    private final Positions[] A_RUSH = {Positions.A_SITE, Positions.A_SITE, Positions.A_SITE, Positions.A_SITE, Positions.A_SITE};
+    private final Positions[] B_SPLIT = {Positions.MID_TO_B, Positions.MID_TO_B, Positions.MID_TO_B, Positions.B_TUNNELS, Positions.B_TUNNELS};
+    private final Positions[] B_RUSH = {Positions.B_SITE, Positions.B_SITE, Positions.B_SITE, Positions.B_SITE, Positions.B_SITE};
+    private final Positions[] DEFAULT = {Positions.B_SITE, Positions.MID_TO_B, Positions.A_SHORT, Positions.A_LONG, Positions.A_SITE};
 
+    
+    public Positions[] getCallouts(Callouts co) {
+    	switch(co) {
+    	case A_SPLIT:
+    		return A_SPLIT;
+    	case A_RUSH:
+    		return A_RUSH;
+    	case B_SPLIT:
+    		return B_SPLIT;
+    	case B_RUSH:
+    		return B_RUSH;
+    	case DEFAULT:
+    		return DEFAULT;
+    	default:
+    		return null;
+    	}
     }
 
     public ArrayList<GridPoint> getPosition(Positions position, T type){
